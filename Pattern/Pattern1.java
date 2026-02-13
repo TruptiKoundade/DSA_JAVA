ec1d56a1054625d515f3a9cdcd64fb567731599c
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-13 20:00:03 +0530

    initial commit

---
+++ Pattern/Pattern1.java
@@ -0,0 +1,17 @@
+package Pattern;
+import java.util.*;
+class Pattern1 {
+    public static void main(String args[])
+    {
+        int n=5;
+        int m=4;
+        for(int i=0;i<n;i++)
+        {
+          for(int j=0;j<m;j++)
+          {
+            System.out.print("*");
+          }
+          System.out.println();
+        }
+    }
+}
