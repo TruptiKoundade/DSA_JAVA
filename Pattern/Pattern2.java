2961d474f86af820ab88cb502a28ca18ce8bd0c5
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-14 00:09:43 +0530

    Update Pattern2.java

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -3,8 +3,8 @@ package Pattern;
 public class Pattern2 {
     public static void main(String args[])
     {
-        int n=5;
-        int m=4;
+        int n=4;
+        int m=5;
        for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
