ad6b93c00406ef51b8dc46ae8a9f9795216ed334
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-15 00:24:38 +0530

    added pattern15

---
+++ Pattern/Pattern15.java
@@ -0,0 +1,26 @@
+package Pattern;
+
+class Pattern15
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            for(char ch='A';ch<='A'+i;ch++)
+            {
+              System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+OUTPUT:
+A 
+A B
+A B C
+A B C D
+A B C D E 
+*/
