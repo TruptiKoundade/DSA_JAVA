e16c4eb7586d8ec5873b8f492594af4583cfbb2f
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-17 23:27:41 +0530

    Recursion5 added

---
+++ Recursion/SumOfFirstNnumbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class SumOfFirstNnumbers {
+    void f(int i,int sum)
+    {
+        if(i<1)
+        {
+            System.out.print(sum);
+            return;
+        }
+        f(i-1,sum+i);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        SumOfFirstNnumbers r1=new SumOfFirstNnumbers();
+        System.out.print("\nEnter n:");
+        int n=sc.nextInt();
+        r1.f(n,0);
+    }
+}
+/*
+OUTPUT:
+Enter n:5
+15 
+*/
