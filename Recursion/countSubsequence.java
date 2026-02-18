a2733332fba481ee52ebaeb7280588a46b61c7ce
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-18 10:49:25 +0530

    Recursion15 added

---
+++ Recursion/countSubsequence.java
@@ -0,0 +1,35 @@
+public class countSubsequence {
+    int count(int ind,int s,int sum,int arr[],int n)
+    {
+      if(ind==n)
+        {
+            if(s==sum)
+            {
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        } 
+        s+=arr[ind];
+        int l=count(ind+1,s,sum,arr,n);
+        
+        s-=arr[ind];
+        int r=count(ind+1,s,sum,arr,n);
+
+        return l+r;
+    }
+    public static void main(String args[])
+    {
+        countSubsequence c=new countSubsequence();
+        int arr[]={1,2,1};
+        int n=arr.length;
+        int sum=2;
+        int count=c.count(0,0,sum,arr,n);
+        System.out.println(sum);
+    }
+}
+/*
+OUTPUT:
+2
+ */
