fe6af3b34c01ff185d2e020df2c2afaf52985878
Trupti Koundade <koundadetrupti@gmail.com>
2026-02-18 10:35:33 +0530

    Recursion14 added

---
+++ Recursion/PrintAnysubsequence.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class PrintAnysubsequence {
+
+    boolean print(int ind, ArrayList<Integer> ds, int s, int sum, int arr[], int n) {
+        if (ind == n) {
+            if (s == sum) {
+                for (int val : ds) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+                return true;   // found one valid subsequence
+            }
+            return false;
+        }
+
+        // take the element
+        ds.add(arr[ind]);
+        s += arr[ind];
+        if (print(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        // not take the element
+        s -= arr[ind];
+        ds.remove(ds.size() - 1);
+        if (print(ind + 1, ds, s, sum, arr, n)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String args[]) {
+        PrintAnysubsequence p1 = new PrintAnysubsequence();
+        int arr[] = {1, 2, 1};
+        int n = arr.length;
+        int sum = 2;
+
+        ArrayList<Integer> ds = new ArrayList<>();
+        p1.print(0, ds, 0, sum, arr, n);
+    }
+}
